9cec407979c21c6f73ab2486a22e4230f0da7166
dsmith <dsmith@verisign.com>
2014-11-16 12:01:17 -0500

    config

--- src/python/test.py
+++ src/python/test.py
@@ -14,23 +14,13 @@ CONFIG_FILE='config/settings.ini'
 print "Config: {} exists? {}".format(CONFIG_FILE, os.path.isfile(CONFIG_FILE))
 Config = ConfigParser.ConfigParser()
 Config.read(CONFIG_FILE)
-token=Config.get('auth','token')
+client_secret=Config.get('auth','clientSecret')
 client_id=Config.get('auth', 'clientID')
-print token
-
-scope = 'user-library-read'
-username='polestarxyz'
-#username, scope=None, client_id=None, client_secret=None, redirect_uri=None
-#spot = spotipy.Spotify(auth=token)
-#util.prompt_for_user_token(username, scope=scope, client_id=client_id, client_secret=token)
-#results = spot.search(q='wolfhounds', limit=20)
-#print json.dumps(results, sort_keys=True, indent=2, separators=(',', ': '))
-#for i, t in enumerate(results['tracks']['items']):
-#    print ' ', i, t['name']
+username=Config.get('auth','user')
 
 body = {'grant_type': 'client_credentials'}
 body_data = json.dumps(body)
-auth_raw = client_id + ':' + token
+auth_raw = client_id + ':' + client_secret
 auth_encoded = base64.b64encode(auth_raw)
 print "{} = {}".format(auth_raw, auth_encoded)
 headers = {'Authorization': 'Basic %s' % auth_encoded}
