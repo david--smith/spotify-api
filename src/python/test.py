9674d9ff82efe248ea8e10d107b8070e6017f7cc
dsmith <dsmith@verisign.com>
2014-11-17 08:26:42 -0500

    ---

--- src/python/test.py
+++ src/python/test.py
@@ -42,35 +42,40 @@ for song in songs:
 f.write('</table>')
 f.write('</body></html>')
 f.close()
-exit()
+#exit()
 
 
 
+def login_to_spotify():
+  CONFIG_FILE='config/settings.ini'
+  print "Config: {} exists? {}".format(CONFIG_FILE, os.path.isfile(CONFIG_FILE))
+  Config = ConfigParser.ConfigParser()
+  Config.read(CONFIG_FILE)
+  client_secret=Config.get('auth','clientSecret')
+  client_id=Config.get('auth', 'clientID')
+  username=Config.get('auth','user')
 
-CONFIG_FILE='config/settings.ini'
+  ####################
+  # Authorization
+  ####################
+  body = {'grant_type': 'client_credentials', 'scope':'playlist-modify-public playlist-modify-private '}
+  body_data = json.dumps(body)
+  auth_raw = client_id + ':' + client_secret
+  auth_encoded = base64.b64encode(auth_raw)
+  print "{} = {}".format(auth_raw, auth_encoded)
+  headers = {'Authorization': 'Basic %s' % auth_encoded}
+  url = "https://accounts.spotify.com/api/token"
+  print '----------'
+  print 'POST request to: {}\nWith headers: {}\nWith body: {}'.format(url, headers, body_data)
+  r = requests.post(url, data=body, headers=headers)
+  print '----------'
+  print r.status_code, r.content
+  r_json = r.json()
+  print json.dumps(r_json, sort_keys=True, indent=2, separators=(',', ': '))
+  token=r_json['access_token']
+  return token, client_id
 
-print "Config: {} exists? {}".format(CONFIG_FILE, os.path.isfile(CONFIG_FILE))
-Config = ConfigParser.ConfigParser()
-Config.read(CONFIG_FILE)
-client_secret=Config.get('auth','clientSecret')
-client_id=Config.get('auth', 'clientID')
-username=Config.get('auth','user')
-
-body = {'grant_type': 'client_credentials'}
-body_data = json.dumps(body)
-auth_raw = client_id + ':' + client_secret
-auth_encoded = base64.b64encode(auth_raw)
-print "{} = {}".format(auth_raw, auth_encoded)
-headers = {'Authorization': 'Basic %s' % auth_encoded}
-url = "https://accounts.spotify.com/api/token"
-print '----------'
-print 'POST request to: {}\nWith headers: {}\nWith body: {}'.format(url, headers, body_data)
-r = requests.post(url, data=body, headers=headers)
-print '----------'
-print r.status_code, r.content
-r_json = r.json()
-print json.dumps(r_json, sort_keys=True, indent=2, separators=(',', ': '))
-token=r_json['access_token']
+token, client_id = login_to_spotify()
 headers = {'Authorization': 'Bearer %s' % token}
 
 artist = 'wolfhounds'
@@ -80,5 +85,15 @@ print r.status_code, r.content
 print '----------'
 r_json = r.json()
 print json.dumps(r_json['artists']['items'][0])
+
+url = 'https://api.spotify.com/v1/users/{}/playlists'.format(client_id)
+body = {'name': 'foo1'}
+headers = {'Authorization': 'Bearer %s' % token, 'Content-Type': 'application/json'}
+r = requests.post(url, data=body, headers=headers)
+print '----------'
+print r.status_code, r.content
+r_json = r.json()
+print json.dumps(r_json, sort_keys=True, indent=2, separators=(',', ': '))
+
 
     ##################################
