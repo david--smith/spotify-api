331f103b8cf64b95009cfbd857488d79e17b0607
dsmith <dsmith@verisign.com>
2014-11-17 08:38:04 -0500

    ---

--- src/python/test.py
+++ src/python/test.py
@@ -11,41 +11,6 @@ import base64
 
 from bs4 import BeautifulSoup
 
-url = 'http://wprb.com/tpm/world/printplaylist.php?show_id=32977'
-r = requests.get(url)
-print r.content
-regex = re.compile("<td class='mid'.*>.*>(.*)</span>",re.MULTILINE)
-regex_result = regex.search(r.content)
-print regex.findall(r.content)
-print '-----------'
-soup = BeautifulSoup(r.content)
-trs = soup.find_all('tr')
-songs = []
-for tr in trs:
-  song = [text for text in tr.stripped_strings]
-  if len(song) > 0 and song[0] is not 'Artist':
-    songs.append(song)
-
-f = open('./output/test.html','w')
-f.write('<html><body>\n')
-f.write('<table border=1>')
-for song in songs:
-  artist = song[0]
-  track = song[1]
-  album = song[2]
-  print artist + ' // ' + track + ' // ' + album
-  f.write('<tr align=left valign=top>\n')
-  f.write('  <td>' + artist + '</td>\n')
-  f.write('  <td>' + track + '</td>\n')
-  f.write('  <td>' + album + '</td>\n')
-  f.write('</tr>\n')
-f.write('</table>')
-f.write('</body></html>')
-f.close()
-#exit()
-
-
-
 def login_to_spotify():
   CONFIG_FILE='config/settings.ini'
   print "Config: {} exists? {}".format(CONFIG_FILE, os.path.isfile(CONFIG_FILE))
@@ -75,16 +40,62 @@ def login_to_spotify():
   token=r_json['access_token']
   return token, client_id
 
+def fetch_artist(artist):
+  artist = 'wolfhounds'
+  url = 'https://api.spotify.com/v1/search?q={}&type=artist'.format(artist)
+  r = requests.get(url, headers=headers)
+  #print r.status_code, r.content
+  #print '----------'
+  r_json = r.json()
+  #print json.dumps(r_json['artists']['items'][0])
+  artist_matches = [artist_info for artist_info in r_json['artists']['items'] if artist_info['name'].lower() == artist.lower()]
+  return artist_matches
+
+def fetch_wprb_playlist(url):
+  r = requests.get(url)
+  #print r.content
+  regex = re.compile("<td class='mid'.*>.*>(.*)</span>",re.MULTILINE)
+  regex_result = regex.search(r.content)
+  #print regex.findall(r.content)
+  #print '-----------'
+  soup = BeautifulSoup(r.content)
+  trs = soup.find_all('tr')
+  songs = []
+  for tr in trs:
+    song = [text for text in tr.stripped_strings]
+    if len(song) > 0 and song[0] is not 'Artist':
+      songs.append(song)
+
+  f = open('./output/test.html','w')
+  f.write('<html><body>\n')
+  f.write('<table border=1>')
+  for song in songs:
+    artist = song[0]
+    track = song[1]
+    album = song[2]
+    print artist + ' // ' + track + ' // ' + album
+    f.write('<tr align=left valign=top>\n')
+    f.write('  <td>' + artist + '</td>\n')
+    f.write('  <td>' + track + '</td>\n')
+    f.write('  <td>' + album + '</td>\n')
+    f.write('</tr>\n')
+  f.write('</table>')
+  f.write('</body></html>')
+  f.close()
+
+######################################################################
+
 token, client_id = login_to_spotify()
 headers = {'Authorization': 'Bearer %s' % token}
 
-artist = 'wolfhounds'
-url = 'https://api.spotify.com/v1/search?q={}&type=artist'.format(artist)
-r = requests.get(url, headers=headers)
-print r.status_code, r.content
-print '----------'
-r_json = r.json()
-print json.dumps(r_json['artists']['items'][0])
+url = 'http://wprb.com/tpm/world/printplaylist.php?show_id=32977'
+fetch_wprb_playlist(url)
+
+wolves = fetch_artist('wolfhounds')
+wolf = wolves[0]
+print wolf['name'] + ' == ' + wolf['uri']
+print json.dumps(wolf, sort_keys=True, indent=2, separators=(',', ': '))
+
 
 url = 'https://api.spotify.com/v1/users/{}/playlists'.format(client_id)
 body = {'name': 'foo1'}
